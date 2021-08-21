8562583b319dbe18902345fe55545840d12f7e08
colineckert <colineckert10@gmail.com>
2021-08-20 22:26:35 -0700

    build index files

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,15 @@
+//  Import the React and ReactDOM libraries
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Create a react component
+const App = () => {
+  return <div>Hi there!</div>;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Take the react component and show it on screen
+ReactDom.render(<App />, document.querySelector('#root'));
+
+if (module.hot) {
+  module.hot.accept();
+}
