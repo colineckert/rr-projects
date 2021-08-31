77e9419afea527a38ca751a1b284a56ffbb5c8fe
colineckert <colineckert10@gmail.com>
2021-08-31 08:36:30 -0700

    Creat approvalcard template

--- components/src/index.js
+++ components/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import CommentDetail from './CommentDetail';
 import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard />
       <CommentDetail
         author="Sam"
         timeAgo="Today at 4:45PM"
