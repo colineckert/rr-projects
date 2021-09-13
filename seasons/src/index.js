d32189696a9906e0ff60738313b31101f68e6a55
colineckert <colineckert10@gmail.com>
2021-09-12 20:29:52 -0700

    Set state to user lat

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,15 +5,18 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
+    // the only time we do direct assignment to this.state
     this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
   }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-
     return <div>Latitude: {this.state.lat}</div>;
   }
 }
