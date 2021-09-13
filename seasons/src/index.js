d8620adb6e5f33154f60df275827dae4973f8a53
colineckert <colineckert10@gmail.com>
2021-09-13 05:36:59 -0700

    Add cdm and move fetch

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,17 +5,18 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    // the only time we do direct assignment to this.state
     this.state = { lat: null, errorMessage: '' };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('My component was just updated - it rerendered!');
   }
 
   render() {
