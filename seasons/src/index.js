918c170b10bf687a1e655288397d9a0bf90b9e60
colineckert <colineckert10@gmail.com>
2021-09-12 20:43:56 -0700

    Conditionally render laoding

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -6,18 +6,28 @@ class App extends React.Component {
     super(props);
 
     // the only time we do direct assignment to this.state
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: '' };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading...</div>;
   }
 }
 
