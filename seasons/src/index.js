254657e63b265bd3fc6bdeec8269fbe17e5ce306
colineckert <colineckert10@gmail.com>
2021-09-19 11:01:16 -0700

    Add renderContent to avoid many returns

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -13,7 +13,7 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -23,6 +23,10 @@ class App extends React.Component {
     }
 
     return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
