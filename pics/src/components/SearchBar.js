0588e140dfe4c7a5738876730e841d7a303cd3b7
colineckert <colineckert10@gmail.com>
2021-09-20 20:09:09 -0700

    create input change callback

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  onInputChange(e) {
+    console.log(e.target.value);
+  }
+
   render() {
     return (
       <div className="io segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input type="text" onChange={this.onInputChange} />
           </div>
         </form>
       </div>
