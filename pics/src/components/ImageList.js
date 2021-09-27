915a0b2373c05d1215ac9ab724eaf97531ae7622
colineckert <colineckert10@gmail.com>
2021-09-27 08:35:00 -0700

    Render ImageList

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const ImageList = (props) => {
-  console.log(props.images);
+  const images = props.images.map(({ id, urls, alt_description }) => {
+    return <img key={id} src={urls.regular} alt={alt_description} />;
+  });
 
-  return <div>Image List</div>;
+  return <div>{images}</div>;
 };
 
 export default ImageList;
