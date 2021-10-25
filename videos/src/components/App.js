11859d9c9d4af8c0155f2a3f35129d2677b4792d
colineckert <colineckert10@gmail.com>
2021-10-24 20:33:34 -0700

    Display num videos

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -3,6 +3,8 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
 export default class App extends Component {
+  state = { videos: [] };
+
   onFormSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
@@ -10,13 +12,14 @@ export default class App extends Component {
       },
     });
 
-    console.log(response);
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onFormSubmit} />
+        <SearchBar onFormSubmit={this.onFormSubmit} />I have{' '}
+        {this.state.videos.length} videos
       </div>
     );
   }
