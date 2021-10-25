d33464e684b9081b04b91b3d688b0ea89271af02
colineckert <colineckert10@gmail.com>
2021-10-24 20:19:01 -0700

    Create youtube api

---
+++ videos/src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyDxFZLX_vm_VsT-IU3aP7rUWzuoKFlg7J8';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY,
+  },
+});
