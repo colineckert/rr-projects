c0c535f8c2fc1089859ac172b6b0376e4ff4d914
colineckert <colineckert10@gmail.com>
2021-11-09 21:25:50 -0800

    Fix errors

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -5,5 +5,17 @@ export default function VideoDetail({ video }) {
     return <div>Loading...</div>;
   }
 
-  return <div>{video.snippet.title}</div>;
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} title="video player" />
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
 }
