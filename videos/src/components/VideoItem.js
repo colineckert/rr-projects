cda7996aa0e69aaf723e9dd96bca94b009a3ecce
colineckert <colineckert10@gmail.com>
2021-11-09 21:08:07 -0800

    Pass onVideoSelect callback prop

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,9 +1,9 @@
 import './VideoItem.css';
 import React from 'react';
 
-export default function VideoItem({ video }) {
+export default function VideoItem({ video, onVideoSelect }) {
   return (
-    <div className="video-item item">
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
       <img
         className="ui image"
         src={video.snippet.thumbnails.medium.url}
--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-export default function VideoList({ videos }) {
+export default function VideoList({ videos, onVideoSelect }) {
   const renderedList = videos.map((video) => {
-    return <VideoItem video={video} />;
+    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
   });
 
   return <div className="ui relaxed divided list">{renderedList}</div>;
