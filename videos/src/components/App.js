27bddeb40fd90cc3c26b4ae41c3c9f08573f9937
colineckert <colineckert10@gmail.com>
2021-11-16 19:03:11 -0800

    default search

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -7,6 +7,10 @@ import VideoDetail from './VideoDetail';
 export default class App extends Component {
   state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.onFormSubmit('buildings');
+  }
+
   onFormSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
@@ -14,7 +18,10 @@ export default class App extends Component {
       },
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
 
   onVideoSelect = (video) => {
