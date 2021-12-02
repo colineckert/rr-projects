fa9fb3d3094c35d38c66adfc5d8bf3682bc036e9
colineckert <colineckert10@gmail.com>
2021-12-01 21:01:55 -0800

    Make wiki call

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,7 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Search() {
   const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    const search = async () => {
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+    };
+
+    search();
+  }, [term]);
 
   return (
     <div>
