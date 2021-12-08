7208656c38481396db429ba8e5a5f7a9717f2455
colineckert <colineckert10@gmail.com>
2021-12-07 20:47:09 -0800

    Throttle api calls with useEffect

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -20,7 +20,19 @@ export default function Search() {
       setResults(data.query.search);
     };
 
-    search();
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 1000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
   }, [term]);
 
   const renderedResults = results.map((result) => {
