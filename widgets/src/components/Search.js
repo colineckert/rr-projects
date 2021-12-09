b35cfe304c54c308d5ddf8a860ae2ddb5f5d3ddb
colineckert <colineckert10@gmail.com>
2021-12-08 18:59:16 -0800

    Add second useEffect for debounceTerm

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -3,7 +3,18 @@ import axios from 'axios';
 
 export default function Search() {
   const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  });
 
   useEffect(() => {
     const search = async () => {
@@ -13,27 +24,15 @@ export default function Search() {
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
 
       setResults(data.query.search);
     };
 
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 1000);
-
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
-  }, [term]);
+    search();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return (
