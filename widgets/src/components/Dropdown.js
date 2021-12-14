7c1961f1af9044509cb3bc465cdb749f91a60881
colineckert <colineckert10@gmail.com>
2021-12-13 20:35:11 -0800

    solve click to close dropdown

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,7 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Dropdown = ({ options, selected, onSelectionChange }) => {
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    document.body.addEventListener(
+      'click',
+      () => {
+        setOpen(false);
+      },
+      { capture: true }
+    );
+  }, []);
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
