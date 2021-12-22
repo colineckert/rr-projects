fdb4d152fe281647bd569f9b06888fc83af70d02
colineckert <colineckert10@gmail.com>
2021-12-21 20:02:42 -0800

    transalte text

--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Convert';
 
 const options = [
   {
@@ -34,6 +35,9 @@ const Translate = () => {
         onSelectionChange={setLanguage}
         options={options}
       />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert text={text} language={language} />
     </div>
   );
 };
