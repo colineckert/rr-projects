5181ff6fdf190728ea2c7049d41875ae07e359db
colineckert <colineckert10@gmail.com>
2022-01-09 14:58:01 -0800

    use thunk middleware

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,10 @@
-export const fetchPosts = () => {
-  return {
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get('/posts');
+
+  dispatch({
     type: 'FETCH_POSTS',
-  };
+    payload: response,
+  });
 };
