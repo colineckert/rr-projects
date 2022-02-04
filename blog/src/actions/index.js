517af40b07f407a2ddb0e7a1ecf68e88d8720a6f
colineckert <colineckert10@gmail.com>
2022-02-03 20:05:02 -0800

    memoize fetchUser

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async (dispatch) => {
@@ -6,8 +7,12 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (userId) => async (dispatch) => {
+export const fetchUser = (userId) => (dispatch) => {
+  _fetchUser(userId, dispatch);
+};
+
+const _fetchUser = _.memoize(async (userId, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${userId}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
-};
+});
