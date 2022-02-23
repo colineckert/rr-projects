f01dcc193854a6a7d853e559c8d55967e4a51aad
colineckert <colineckert10@gmail.com>
2022-02-22 19:59:16 -0800

    Stub header

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -5,11 +5,13 @@ import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
+        <Header />
         <div>
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
