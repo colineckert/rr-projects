9dc7df8495a6861bb52f20caef98d068042c6468
colineckert <colineckert10@gmail.com>
2022-02-22 20:27:45 -0800

    load auth2

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 export class GoogleAuth extends Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId:
+          '497692444496-4jc5ssbnf8808ned728serpruuad8l9o.apps.googleusercontent.com',
+        scope: 'email',
+      });
+    });
+  }
+
   render() {
     return <div>GoogleAuth</div>;
   }
