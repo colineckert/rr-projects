c9dcb4ca414d12037cd7a59c23a219f13b6dae2b
colineckert <colineckert10@gmail.com>
2022-02-24 18:54:39 -0800

    check if signed in

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 
-export class GoogleAuth extends Component {
+class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId:
-          '497692444496-4jc5ssbnf8808ned728serpruuad8l9o.apps.googleusercontent.com',
-        scope: 'email',
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            '497692444496-4jc5ssbnf8808ned728serpruuad8l9o.apps.googleusercontent.com',
+          scope: 'email',
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
 
+  renderAuthButton() {
+    const { isSignedIn } = this.state;
+
+    if (isSignedIn === null) {
+      return <div>I don't know if we are signed in</div>;
+    } else if (isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
   render() {
-    return <div>GoogleAuth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
