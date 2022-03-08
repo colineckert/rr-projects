66b7a0962edecefd15e60bb553324852aba77ff5
colineckert <colineckert10@gmail.com>
2022-03-07 21:08:41 -0800

    hook up form inputs

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends Component {
+  renderInput({ input }) {
+    return <input {...input} />;
+  }
 
-export default StreamCreate;
+  render() {
+    console.log(this.props);
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'streamCreate',
+})(StreamCreate);
