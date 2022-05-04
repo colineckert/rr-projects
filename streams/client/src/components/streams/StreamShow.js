7d47b1158589a4eb0c5610af3454324a1cfe9356
colineckert <colineckert10@gmail.com>
2022-05-04 09:24:19 -0700

    Install flv and show video player

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,8 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = createRef();
+  }
+
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
@@ -16,6 +23,7 @@ class StreamShow extends Component {
 
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
