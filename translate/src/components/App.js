0ed45494c9c1f0c0a08ebbf6290aa177bbd9800c
colineckert <colineckert10@gmail.com>
2022-05-14 21:42:05 -0700

    setup flags and click handlers

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 
 export class App extends Component {
+  state = { language: 'english' };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+
   render() {
-    return <div className="ui container">App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange('dutch')}
+          />
+        </div>
+        {this.state.language}
+      </div>
+    );
   }
 }
 
