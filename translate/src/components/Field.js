83f9576a4153e52e86dd5915a33e977a85356270
colineckert <colineckert10@gmail.com>
2022-05-23 20:38:10 -0700

    Render specific text based on context

--- translate/src/components/Field.js
+++ translate/src/components/Field.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 export class Field extends Component {
+  static contextType = LanguageContext;
+
   render() {
+    const text = this.context === 'english' ? 'Name' : 'Naam';
     return (
       <div className="ui field">
-        <label>Name</label>
+        <label>{text}</label>
         <input />
       </div>
     );
