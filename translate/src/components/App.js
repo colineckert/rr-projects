62faa199c5631ed9eceb32de05e4df6c23bf9631
colineckert <colineckert10@gmail.com>
2022-05-24 21:24:08 -0700

    refactor to LanguageSelector

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
 
 export class App extends Component {
   state = { language: 'english' };
@@ -13,17 +14,7 @@ export class App extends Component {
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a language:
-          <i
-            className="flag us"
-            onClick={() => this.onLanguageChange('english')}
-          />
-          <i
-            className="flag nl"
-            onClick={() => this.onLanguageChange('dutch')}
-          />
-        </div>
+        <LanguageSelector onLanguageChange={this.onLanguageChange} />
         <ColorContext.Provider value="red">
           <LanguageContext.Provider value={this.state.language}>
             <UserCreate />
---
+++ translate/src/components/LanguageSelector.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export class LanguageSelector extends Component {
+  render() {
+    return (
+      <div>
+        Select a language:
+        <i
+          className="flag us"
+          onClick={() => this.props.onLanguageChange('english')}
+        />
+        <i
+          className="flag nl"
+          onClick={() => this.props.onLanguageChange('dutch')}
+        />
+      </div>
+    );
+  }
+}
+
+export default LanguageSelector;
